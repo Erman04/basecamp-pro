695633150c0f976503cec3728164ca14cec13def
Erman04 <ermankoshkarbaev@gmail.com>
2022-12-12 16:53:27 +0500

    basecamp

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_03_050635) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_12_115207) do
+  create_table "articles", force: :cascade do |t|
+    t.string "title"
+    t.string "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "projects", force: :cascade do |t|
     t.string "name"
     t.text "discription"
